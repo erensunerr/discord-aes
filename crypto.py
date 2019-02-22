6f2fca22b04e2d6a123500a4c967bb77c05458eb
Eren Suner <gerensuner@gmail.com>
2019-02-22 08:41:14 -0500

    Crypto module added

---
+++ crypto.py
@@ -0,0 +1,23 @@
+import gnupg
+#INFO: Crypto module will be multithreaded (another thread)
+#INFO: GNUPG-py docs -> https://pythonhosted.org/python-gnupg/
+class crypto:
+    def __init__():
+        #TODO: Code init and os detection + chdir accordingly
+        pass
+
+    def set_public_key():
+        #TODO: code this
+        pass
+
+    def set_private_key():
+        #TODO: code this
+        pass
+
+    def encrypt():
+        #TODO: code this
+        pass
+
+    def decrypt():
+        #TODO: code this
+        pass
